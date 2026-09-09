4c5f1dde6f7947fa5ce0df95796dc954053a4cbe
ImL1s <ImL1s@users.noreply.github.com>
2026-09-10 01:09:36 +0800

    feat(pid): evaluate FLOAT64 as IEEE754 binary64 in the Torque formula dialect (#236)
    Wiki FLOAT64(A:B:C:D:E:F:G:H) is an IEEE754 float from eight 0-255
    inputs. FLOAT64(63:240:0:0:0:0:0:0) is 1.0. Not FLOAT32.
    INT16 stays unsupportedConstruct.
    
    Refs #79

--- test/pid_compatibility/formula_preflight_test.dart
+++ test/pid_compatibility/formula_preflight_test.dart
@@ -39,6 +39,8 @@ void main() {
     expect(FormulaEngine.preflight('FLOAT32((A-1):B:C:D)'), isNull);
     expect(FormulaEngine.preflight('INT(A)'), isNull);
     expect(FormulaEngine.preflight('INT((A-1))'), isNull);
+    expect(FormulaEngine.preflight('FLOAT64(A:B:C:D:E:F:G:H)'), isNull);
+    expect(FormulaEngine.preflight('FLOAT64((A-1):B:C:D:E:F:G:H)'), isNull);
   });
 
   test('named Torque wiki functions are unsupportedConstruct, not a typo', () {
